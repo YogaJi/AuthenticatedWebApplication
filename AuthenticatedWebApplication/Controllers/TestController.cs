fa80e21f5c0c922ebc3f3d259383daa2201bba3c
YogaJi <y.disturbia@gmail.com>
2022-03-30 04:33:11 +0800

    Add  authorization for secret page
    Add authorization for secret page.
    Remember to confirm email when register.

--- AuthenticatedWebApplication/Controllers/TestController.cs
+++ AuthenticatedWebApplication/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace AuthenticatedWebApplication.Controllers
 {
     public class TestController : Controller
     {
+        [Authorize]
         public IActionResult Secret()
         {
             return View();
