c03bf3cca8f36ec2606db59dc6850d831c8ae3db
YogaJi <y.disturbia@gmail.com>
2022-03-29 14:49:47 +0800

    add scaffold

---
+++ AuthenticatedWebApplication/Areas/Identity/Data/AuthenticatedWebApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticatedWebApplication.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticatedWebApplication.Data
+{
+    public class AuthenticatedWebApplicationContext : IdentityDbContext<AuthenticatedWebApplicationUser>
+    {
+        public AuthenticatedWebApplicationContext(DbContextOptions<AuthenticatedWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ AuthenticatedWebApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AuthenticatedWebApplication.Areas.Identity.Data;
+using AuthenticatedWebApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthenticatedWebApplication.Areas.Identity.IdentityHostingStartup))]
+namespace AuthenticatedWebApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthenticatedWebApplicationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthenticatedWebApplicationContextConnection")));
+
+                services.AddDefaultIdentity<AuthenticatedWebApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthenticatedWebApplicationContext>();
+            });
+        }
+    }
+}
